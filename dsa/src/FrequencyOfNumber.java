0ea9b132b94c731980e41ef4be40898f5977549f
PrateekDoliya <prateekdoliya00@gmail.com>
2022-05-03 18:42:50 +0530

    Add getFreq method to FrequencyOfNumber

--- dsa/src/FrequencyOfNumber.java
+++ dsa/src/FrequencyOfNumber.java
@@ -1,4 +1,6 @@
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class FrequencyOfNumber {
 
@@ -21,6 +23,28 @@ public class FrequencyOfNumber {
 		
 	}
 	
+	public static void getFreq( int ary[] ) {
+		Map<Integer, Integer> map = new TreeMap<>();
+		
+//		serch value -- putiing or increment
+		
+		for( int i = 0 ; i < ary.length ; i++ ) {
+			if(map.containsKey(ary[i])) {
+				map.put( ary[i], map.get(ary[i]) + 1 );
+			} else {
+				map.put(ary[i], 1);
+			}
+//			System.out.println(map);
+		}
+		
+		// traverse map
+		for(Map.Entry<Integer, Integer> entry : map.entrySet() ) {
+			System.out.println(entry.getKey()+ " frequency is : " + entry.getValue());
+		}
+	}
+	
+	
+	
 	public static void getStrFrequency( String str) {
 		String ary[] = str.split("");
 		int counter;
@@ -40,11 +64,12 @@ public class FrequencyOfNumber {
 	
 	public static void main(String[] args) {
 		
-		int ary[] = {2,5,4,8,1,3,4,9,2,3,2};
-		FrequencyOfNumber.getFreguency(ary);
+		int ary[] = {2,5,5,4,5,6,3,7,3,1,7};
+//		FrequencyOfNumber.getFreguency(ary);
+		FrequencyOfNumber.getFreq(ary);
 		
-		String string = "innogent";
-		FrequencyOfNumber.getStrFrequency(string);
+//		String string = "innogent";
+//		FrequencyOfNumber.getStrFrequency(string);
 		
 	}
 	
